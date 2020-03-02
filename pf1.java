ad1140686d2b39ca8b299d4b08896782bd6d837c
Heila Al-Mogren <55871567+Heila-Almogren@users.noreply.github.com>
2020-03-02 13:02:14 +0300

    Add files via upload

---
+++ pf1.java
@@ -0,0 +1,20 @@
+
+Filter1.java: 
+package pf;
+import java.io.*;
+public class Filter1 extends Thread{
+PipedWriter myPw;
+public Filter1 (PipedWriter pw) {
+myPw-pw;
+}
+public void run() {
+int j;
+try {
+File x = new File(inputFile);
+InputStream IS = InputStream
+}
+catch (Exception e){
+
+}
+}
+}
